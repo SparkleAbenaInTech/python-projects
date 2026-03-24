79b4937e668f5995c4b75d37dd1f33e793a3ffc0
SparkleAbenaInTech <aapau01@icloud.com>
2026-03-23 19:37:48 -0700

    day 3 - variables and user input

---
+++ day3.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+goal = input("What is your biggest goal? ")
+
+print("Hello " + name + "!")
+print("At " + age + " years old you are just getting started!")
+print("Your goal is: " + goal)
+print("In 120 days you will be a Full Stack AI Engineer!")
+
+
+
